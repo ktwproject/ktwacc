29c075732e584c1e71a2fa4306dd463c5db102b2
KTG\phansa <lekgaetonic>
2023-03-30 08:52:58 +0700

    fix nodejs

--- index.js
+++ index.js
@@ -1,17 +1,49 @@
-import { Client } from '@line/bot-sdk';
+'use strict';
 
-const client = new Client({
-  channelAccessToken: 'Y9zCxBmVqtr/pqTFap3ZIv6oPklzoVFWq1lwWW3WkXk5tnwNNoPtE3pAX0K45Ysy0wb0jM70kquurOLcs05OqQF+G94JnYgrnmENKZykNT6YP3bVE9uSZ+744s9kd4AMEUkWCjD+rL76cJCxbmXGfQdB04t89/1O/w1cDnyilFU='
-});
+const line = require('@line/bot-sdk');
+const express = require('express');
 
-const message = {
-  type: 'text',
-  text: 'Hello World!'
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
 };
 
-client.replyMessage('<replyToken>', message)
-  .then(() => {
-  })
-  .catch((err) => {
-    // error handling
-  });
+// create LINE SDK client
+const client = new line.Client(config);
+
+// create Express app
+// about Express itself: https://expressjs.com/
+const app = express();
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
+
+// listen on port
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
